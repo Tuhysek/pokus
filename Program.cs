73a78d348319cbc671e921f85f8748bc93d7f0ac
Tuhysek <104440102+Tuhysek@users.noreply.github.com>
2022-04-26 15:37:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int i = 0; // initialization
+
+            while (i < 10) // condition
+            {
+                Console.WriteLine("i = {0}", i);
+
+                i++; // increment
+            }
+
+
+        }
+    }
+}
